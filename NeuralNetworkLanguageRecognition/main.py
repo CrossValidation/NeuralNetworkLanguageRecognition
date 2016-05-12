0461b47ebf21e5bf4579006b4715383c0f961465
Silvio Messi <messisilvio@gmail.com>
2016-05-12 18:07:10 +0200

    Perceptrons network test.

--- NeuralNetworkLanguageRecognition/main.py
+++ NeuralNetworkLanguageRecognition/main.py
@@ -1,8 +1,27 @@
 from neuralNetworkElements  import Perceptron, Input
 
-if __name__ == '__main__':
+def onePerceptron():
     inputs = [ Input(1), Input(1), Input()]
     perceptron = Perceptron(inputs=inputs)
     print perceptron.inputs
     print perceptron.activeted()
+    
+def perceptronNetwork():
+    input1 = Input(1)
+    input2 = Input(1)
+    input3 = Input()
+    input4 = Input()
+    perceptron1 = Perceptron(inputs=[input1, input2])
+    perceptron2 = Perceptron(inputs=[input3, input4])
+    perceptron3 = Perceptron(inputs=[perceptron1, perceptron2])
+    print perceptron1.inputs
+    print perceptron2.inputs
+    print perceptron3.inputs
+    print perceptron3.activeted()
+    
+    
+if __name__ == '__main__':
+    # onePerceptron()
+    perceptronNetwork()
+   
     
