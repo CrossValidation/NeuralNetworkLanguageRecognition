16162e30a00d2dc909d8d6059e3f9554ba540f53
Lorenzo Fenili <fenili92@gmail.com>
2016-06-09 15:15:07 +0200

    Fixed some bugs

--- NeuralNetworkLanguageRecognition/encoder.py
+++ NeuralNetworkLanguageRecognition/encoder.py
@@ -2,43 +2,44 @@
 import os
 import csv
 from NeuralNetworkLanguageRecognition.wordAndValue import wordAndValue
+from string import lower
 
 class Encoder(object):
     '''
     classdocs
     '''
-    dict = {"&": "00000",
-            "a": "00001",
-            "b": "00010",
-            "c": "00011",
-            "d": "00100",
-            "e": "00101",
-            "f": "00110",
-            "g": "00111",
-            "h": "01000",
-            "i": "01001",
-            "j": "01010",
-            "k": "01011",
-            "l": "01100",
-            "m": "01101",
-            "n": "01110",
-            "o": "01111",
-            "p": "10000",
-            "q": "10001",
-            "r": "10010",
-            "s": "10011",
-            "t": "10100",
-            "u": "10101",
-            "v": "10110",
-            "x": "10111",
-            "w": "11000",
-            "y": "11001",
-            "z": "11010",
-            "à": "11011",
-            "è": "11100",
-            "ì": "11101",
-            "ò": "11110",
-            "ù": "11111"           
+    dict = {"&".decode('utf-8'): "00000",
+            "a".decode('utf-8'): "00001",
+            "b".decode('utf-8'): "00010",
+            "c".decode('utf-8'): "00011",
+            "d".decode('utf-8'): "00100",
+            "e".decode('utf-8'): "00101",
+            "f".decode('utf-8'): "00110",
+            "g".decode('utf-8'): "00111",
+            "h".decode('utf-8'): "01000",
+            "i".decode('utf-8'): "01001",
+            "j".decode('utf-8'): "01010",
+            "k".decode('utf-8'): "01011",
+            "l".decode('utf-8'): "01100",
+            "m".decode('utf-8'): "01101",
+            "n".decode('utf-8'): "01110",
+            "o".decode('utf-8'): "01111",
+            "p".decode('utf-8'): "10000",
+            "q".decode('utf-8'): "10001",
+            "r".decode('utf-8'): "10010",
+            "s".decode('utf-8'): "10011",
+            "t".decode('utf-8'): "10100",
+            "u".decode('utf-8'): "10101",
+            "v".decode('utf-8'): "10110",
+            "x".decode('utf-8'): "10111",
+            "w".decode('utf-8'): "11000",
+            "y".decode('utf-8'): "11001",
+            "z".decode('utf-8'): "11010",
+            "à".decode('utf-8'): "11011",
+            "è".decode('utf-8'): "11100",
+            "ì".decode('utf-8'): "11101",
+            "ò".decode('utf-8'): "11110",
+            "ù".decode('utf-8'): "11111"           
                         }
     firstReadTraining = True
     trainingDataset = {}
@@ -55,16 +56,19 @@ class Encoder(object):
     def translateWord(self, word):
         i = 0
         wordOutput = ""
+        word = word.decode('utf-8')
         if len(word) <= 10:
             while i<10:
-                #x = dict.get(word[i], "pippo")
-                #if  x == 'pippo':
-                    #print(word[i])
                 if(i < len(word)):
                     try:
-                        wordOutput = wordOutput + self.dict[word[i]] #encode word
-                    except KeyError:
+                        wordStr = word[i]
+                        wordOutput = wordOutput + self.dict[wordStr.lower()] #encode word
+                    except Exception as e:
+                        print(e)
+                        
+                        print (word)
                         wordOutput = "default"
+                        print (word[i])
                 else:
                     wordOutput = wordOutput + self.dict['&'] #add padding
                 i += 1
@@ -75,19 +79,21 @@ class Encoder(object):
         if type== 0:
             if self.firstReadTraining == True: 
                 self.firstReadTraining = False
-                file_path = os.path.join(os.path.dirname(__file__), "./dataset/training_dataset.csv")            
+                file_path = os.path.join(os.path.dirname(__file__), "dataset/training_dataset.csv")            
                 with open(file_path, 'r') as csvfile:
                     reader = csv.DictReader(csvfile,delimiter=';')
                     for index,row in enumerate(reader):  
+                        rowprova = row[None][0].decode('utf-8')
                         self.trainingDataset[index] =  modelWordValue = wordAndValue(row[None][0],row[None][1]) 
                         if index >= offset and index < offset + n:
                             #encoded=self.translateWord(row[None][0])
                             self.valueEncoded.append(wordAndValue(self.translateWord(row[None][0]), row[None][1]))
             else:
-                while offset <= offset + n:
-                    self.valueEncoded.append(wordAndValue(self.translateWord(self.trainingDataset[offset].getWord()), self.trainingDataset[offset].getValue()))
-                    offset = offset + 1
-        if type== 1:
+                index_training = offset
+                while index_training <= offset + n:
+                    self.valueEncoded.append(wordAndValue(self.translateWord(self.trainingDataset[index_training].getWord()), self.trainingDataset[index_training].getValue()))
+                    index_training = index_training + 1
+        if type == 1:          
             if self.firstReadValidation == True: 
                 self.firstReadValidation = False
                 file_path = os.path.join(os.path.dirname(__file__), "./dataset/validation_dataset.csv")            
@@ -99,6 +105,8 @@ class Encoder(object):
                             #encoded=self.translateWord(row[None][0])
                             self.valueEncoded.append(wordAndValue(self.translateWord(row[None][0]), row[None][1]))
             else:
-                while offset <= offset + n:
-                    self.valueEncoded.append(wordAndValue(self.translateWord(self.validationDataset[offset].getWord()), self.validationDataset[offset].getValue()))
-                    offset = offset + 1
+                index_training = offset
+                while index_training <= offset + n:
+                    self.valueEncoded.append(wordAndValue(self.translateWord(self.validationDataset[index_training].getWord()), self.validationDataset[index_training].getValue()))
+                    index_training = index_training + 1
+        return self.valueEncoded
--- NeuralNetworkLanguageRecognition/main.py
+++ NeuralNetworkLanguageRecognition/main.py
@@ -2,7 +2,8 @@ from NeuralNetworkLanguageRecognition.encoder import Encoder
 
 if __name__ == '__main__':
     encoder = Encoder()
-    encoder.getWord(0,1,50)
+    
+    encoder.getWord(0,0,50)
     encoder.getWord(0,51,70)
     encoder.getWord(1,1,50)
     encoder.getWord(1,51,70)
